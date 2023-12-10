42a077c340f642357d430ef9883d621cbfdfdb8e
Athiwat Wiroonphattanadate <athiwat.wir@gmail.com>
2023-12-10 18:22:30 +0700

    home cupon

---
+++ resources/views/includes/home.cupon.php
@@ -0,0 +1,105 @@
+<div class="section">
+    <div class="container">
+        <div class="row">
+            <div class="flickity-preloader"
+                data-flickity='{ "autoPlay": true, "cellAlign": "left", "pageDots": true, "prevNextButtons": true, "contain": true, "rightToLeft": false }'>
+
+                <div class="col-12 col-lg-4 mb-4 py-4 me-4">
+                    <div
+                        class="card border-0 shadow-md shadow-3d-hover transition-all-ease-250 transition-hover-top h-100">
+                        <div class="clearfix">
+                            <img class="img-fluid " src="{{asset('assets/images/cupon/a.jpg')}}"
+                                alt="Early Bird 10% Off จองวันนี้-31Dec.23 เดินทาง 1 Jan-30 April"
+                                style="border-radius: 15px 15px 0px 0px;">
+                        </div>
+                        <div class="card-body fw-light bg-main-color" style="border-radius: 0px 0px 15px 15px;">
+                            <div class="d-table">
+                                <div class="d-table-cell align-bottom">
+                                    <span class="f-kanit-400 text-white pb-2"><i class="fi fi-product-tag"></i> Offers
+                                        and Promotions</span>
+                                    <h2 class="h5 card-title mb-4 text-white f-kanit-600">
+                                        Early Bird 10% Off จองวันนี้-31Dec.23 เดินทาง 1 Jan-30 April
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-12 col-lg-4 mb-4 py-4 me-4">
+                    <div
+                        class="card border-0 shadow-md shadow-3d-hover transition-all-ease-250 transition-hover-top h-100">
+                        <div class="clearfix">
+                            <img class="img-fluid " src="{{asset('assets/images/cupon/b.jpg')}}"
+                                alt="Early Bird 10% Off จองวันนี้-31Dec.23 เดินทาง 1 Jan-30 April"
+                                style="border-radius: 15px 15px 0px 0px;">
+                        </div>
+                        <div class="card-body fw-light bg-main-color" style="border-radius: 0px 0px 15px 15px;">
+                            <div class="d-table">
+                                <div class="d-table-cell align-bottom">
+                                    <span class="f-kanit-400 text-white pb-2"><i class="fi fi-product-tag"></i> Offers
+                                        and Promotions</span>
+                                    <h2 class="h5 card-title mb-4 text-white f-kanit-600">
+                                        Multi Island 8% Off จองตั๋วแบบต่อเนื่อง 3 station ขึ้นไป
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-12 col-lg-4 mb-4 py-4 me-4">
+                    <div
+                        class="card border-0 shadow-md shadow-3d-hover transition-all-ease-250 transition-hover-top h-100">
+                        <div class="clearfix">
+                            <img class="img-fluid " src="{{asset('assets/images/cupon/c.jpg')}}"
+                                alt="Round Trip Ticket จองตั๋วไป-กลับ ลด 5%" style="border-radius: 15px 15px 0px 0px;">
+                        </div>
+                        <div class="card-body fw-light bg-main-color" style="border-radius: 0px 0px 15px 15px;">
+                            <div class="d-table">
+                                <div class="d-table-cell align-bottom">
+                                    <span class="f-kanit-400 text-white pb-2"><i class="fi fi-product-tag"></i> Offers
+                                        and Promotions</span>
+                                    <h2 class="h5 card-title mb-4 text-white f-kanit-600">
+                                        Round Trip Ticket จองตั๋วทั้งไป และกลับ ลดทันที 5%
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-12 col-lg-4 mb-4 py-4 me-4">
+                    <div
+                        class="card border-0 shadow-md shadow-3d-hover transition-all-ease-250 transition-hover-top h-100">
+                        <div class="clearfix">
+                            <img class="img-fluid " src="{{asset('assets/images/cupon/d.jpg')}}"
+                                alt="Let's Celebrate 2024 Koh Lipe get 300 Baht discount"
+                                style="border-radius: 15px 15px 0px 0px;">
+                        </div>
+                        <div class="card-body fw-light bg-main-color" style="border-radius: 0px 0px 15px 15px;">
+                            <div class="d-table">
+                                <div class="d-table-cell align-bottom">
+                                    <span class="f-kanit-400 text-white pb-2"><i class="fi fi-product-tag"></i> Offers
+                                        and Promotions</span>
+                                    <h2 class="h5 card-title mb-4 text-white f-kanit-600">
+                                        Let's Celebrate 2024 Koh Lipe get 300 Baht discount
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+            </div>
+        </div>
+    </div>
+</div>
