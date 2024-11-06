b0bca0bbd50459720508159138b761997f7f6cd2
New <aio4t9@protonmail.com>
2024-11-06 11:06:23 +0700

    update

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,51 +1,49 @@
 <!doctype html>
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		@include('includes.head')
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HPSDVHYD39"></script>
-            <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
 
-            gtag('config', 'G-HPSDVHYD39');
-        </script>
+<head>
+    @include('includes.head')
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11355587433"></script>
+    <script>
+        window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-11355587433');
+    </script>
 
-	</head>
-    <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PT752T46"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-    <body>
-		<div id="wrapper">
-			<!-- Header -->
-			<header id="header" class="shadow-xs ">
-				<!-- Navbar -->
-                @include('includes.navbar')
-                <!-- /Navbar -->
-            </header>
-            <!-- /Header -->
+    @yield('script_head')
 
-            <!-- COVER -->
-            @yield('cover-content')
-			<!-- /COVER -->
+</head>
 
-            <div class="section pb-2 pt-2 pt-lg-5 section-main">
-				<div class="container">
-                    @yield('content')
-                </div>
+
+<body>
+    <div id="wrapper">
+        <!-- Header -->
+        <header id="header" class="shadow-xs ">
+            <!-- Navbar -->
+            @include('includes.navbar')
+            <!-- /Navbar -->
+        </header>
+        <!-- /Header -->
+
+        <!-- COVER -->
+        @yield('cover-content')
+        <!-- /COVER -->
+
+        <div class="section pb-2 pt-2 pt-lg-5 section-main">
+            <div class="container">
+                @yield('content')
             </div>
-
-            @yield('section-content')
         </div>
 
-        <!-- Footer -->
-        @include('includes.footer')
-        <!-- /Footer -->
+        @yield('section-content')
+    </div>
 
-        <script src="{{ asset('assets/js/core.min.js') }}"></script>
-        <script src="{{ asset('assets/js/app/app.js') }}?v=@php echo date('YmdHis'); @endphp"></script>
-        @yield('script')
-    </body>
+    <!-- Footer -->
+    @include('includes.footer')
+    <!-- /Footer -->
+
+    <script src="{{ asset('assets/js/core.min.js') }}"></script>
+    <script src="{{ asset('assets/js/app/app.js') }}?v=@php echo date('YmdHis'); @endphp"></script>
+    @yield('script')
+</body>
+
 </html>
--- resources/views/pages/payment/ctsv_response.blade.php
+++ resources/views/pages/payment/ctsv_response.blade.php
@@ -1,15 +1,20 @@
 @extends('layouts.default')
 
+@section('script_head')
+<!-- Event snippet for Page view conversion page -->
+<script>
+    gtag('event', 'conversion', { 'send_to': 'AW-11355587433/8mt2CI6E3eQZEOmG4qYq', 'value': 1.0, 'currency': 'THB' }); 
+</script>
+@stop
+
 @section('content')
 <div class="row min-h-50vh">
     <div class="col-12 text-center">
         <h2 class="{{ $payment_code == 100 ? 'text-success' : 'text-danger' }}">{{ $payment_result }}</h2>
         <div class="d-flex justify-content-center">
             <h4 class="me-1">Redirect to your booking in... </h4>
-            <h4 class="hide timer-countdown text-main-color-2"
-                data-timer-countdown-from="2000"
-                data-timer-countdown-callback-function="autoReturn"
-            ></h4>
+            <h4 class="hide timer-countdown text-main-color-2" data-timer-countdown-from="2000"
+                data-timer-countdown-callback-function="autoReturn"></h4>
         </div>
         <form class="bs-validate" id="booking-record" method="POST" action="{{ route('booking-record') }}">
             @csrf
